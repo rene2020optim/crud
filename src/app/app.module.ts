bfd144bac1952522c78d10c809e742f1ecd4c95e
rene2020optim <75333366+rene2020optim@users.noreply.github.com>
2021-05-13 17:54:44 -0500

    install firebase dependency, add imports and add config firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
+
+const config = {
+  apiKey: "AIzaSyA5NRleAQ1B2yUzow287eSvIML_FpkmE_s",
+  authDomain: "crud-angular-745f4.firebaseapp.com",
+  projectId: "crud-angular-745f4",
+  storageBucket: "crud-angular-745f4.appspot.com",
+  messagingSenderId: "563968918883",
+  appId: "1:563968918883:web:9b3358615b3402927dbf56"
+
+};
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +26,11 @@ import { ListComponent } from './list/list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, 
+    AngularFireAuthModule, 
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
